d3bfa789ee23d99cd4b77d4e6ae0fe2307a6885d
Sancara <santiagocaraballo@gmail.com>
2023-04-15 12:49:24 -0300

    create shots and logic

--- invaders/src/lib.rs
+++ invaders/src/lib.rs
@@ -2,6 +2,8 @@ pub mod frame;
 pub mod render;
 pub mod player;
 pub mod shoot;
+pub mod invaders;
+
 
 pub const NUM_ROWS: usize = 20;
 pub const NUM_COLS: usize = 40;
--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -58,16 +58,23 @@ fn main() -> Result <(), Box<dyn Error>> {
 
     // Game Loop
     let mut player = mut Player::new();
+    let mut instant = Instant::now();
     'gameloop: loop {
         //Per-frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
         let mut curr_frame = new_frame();
-
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
                     KeyCode::Left => player.move_left(),
                     KeyCode::Right => player.move_right(),
+                    KeyCode::Char(" ") | KeyCode::Enter => {
+                        if player.shot() {
+                           audio.play("pew"); 
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop
@@ -76,12 +83,17 @@ fn main() -> Result <(), Box<dyn Error>> {
                 }
             }
         }
-        // Draw & render
-        player.draw(&mut curr_frame);
-        let _ = render_tx.send(curr_frame);
-        thread::sleep(Duration::from_millis(1));
-
     }
+
+
+    // Draw & render
+    player.draw(&mut curr_frame);
+    let _ = render_tx.send(curr_frame);
+    thread::sleep(Duration::from_millis(1));
+
+    // Updates
+    player.update(delta);
+
 
     // Cleanup
     // drop(render_tx); -> for old version of Rust
